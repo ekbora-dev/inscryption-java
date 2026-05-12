7828eee5ece466ffcaeb86d07b5e16208426463e
Salih TANSEL <salih.tansel@etu.unistra.fr>
2026-05-12 12:03:00 +0200

    Classe abstraite Animal v1

--- src/Carte/Animal.java
+++ src/Carte/Animal.java
@@ -1,4 +1,19 @@
 package Carte;
 
 public abstract class Animal {
+    private String m_nom;
+    private int m_pointAttaque;
+    private int m_pointDeVie;
+    private int m_gouttesDeSang;
+    private int m_os;
+    private boolean m_volant;
+
+    public Animal(String nom, int pointAttaque, int pointDeVie, int gouttesDeSang, int os, boolean volant){
+        m_nom = nom;
+        m_pointAttaque = pointAttaque;
+        m_pointDeVie = pointDeVie;
+        m_gouttesDeSang = gouttesDeSang;
+        m_os = os;
+        m_volant = volant;
+    }
 }
